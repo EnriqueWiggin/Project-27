d6c57a04f1bf69a7214cdbf65fc56d00711a8bae
EnriqueWiggin <70081772+EnriqueWiggin@users.noreply.github.com>
2020-10-08 20:59:27 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -75,7 +75,7 @@ function keyPressed()
 {
 if (keyCode === UP_ARROW) {
 
-    	Matter.Body.applyForce(bob1.body,bob1.body.position,{x:100,y:85});
+    	Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-50,y:-60});
     
   	}
 }
